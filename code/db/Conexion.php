0245ff1baa9e4543c1ec606ee19c2b247281fa13
Daniel Ortega <danielnakata@gmail.com>
2016-05-03 22:37:33 -0500

    sadsad
    asdasdasd

--- code/db/Conexion.php
+++ code/db/Conexion.php
@@ -5,15 +5,27 @@
  */
 
     class Conexion {
-        public $host = "localhost";
+//        public $host = "localhost";
+//        public $port = "3306";
+//        public $user = "root";
+//        public $pass = "";
+//        public $name = "dwrentas";
+        
+        public $host = "mysql1.000webhost.com";
         public $port = "3306";
-        public $user = "root";
-        public $pass = "";
-        public $name = "dwrentas";
+        public $user = "a9102384_dwrenta";
+        public $pass = "S02006480d1";
+        public $name = "a9102384_dwrenta";
         
         
+//        function creaConexion(){
+//            return new mysqli($this->host.":".$this->port
+//                    , $this->user
+//                    , $this->pass
+//                    , $this->name);
+//        }
         function creaConexion(){
-            return new mysqli($this->host.":".$this->port
+            return new mysqli($this->host
                     , $this->user
                     , $this->pass
                     , $this->name);
